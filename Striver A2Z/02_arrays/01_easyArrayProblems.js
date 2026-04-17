eb83749b59ad31a094eae49dc0a61f912218afd9
Kul-deep-7 <sahiljangid321@gmail.com>
2026-04-17 11:46:37 +0530

    secondLargestArray

--- Striver A2Z/02_arrays/01_easyArrayProblems.js
+++ Striver A2Z/02_arrays/01_easyArrayProblems.js
@@ -1,5 +1,5 @@
 /* 
-//find largest element in array
+        //find largest element in array
 
 
 function largestElement(array){
@@ -14,3 +14,36 @@ function largestElement(array){
 
 console.log(largestElement([1,3,-9,2,7,8,3]))
  */
+
+/*
+        //Find second Largest number in an array
+
+function secondLargest(arr){
+    if(arr.length < 2) 
+    return "length of an array is less than 2"
+    
+    let largest = arr[0];
+    let secondLargest = null //second largest should be null cuz what if array has all same numbers. if keep it as arr[0] it will return 0th element.
+    
+    for(let i=1; i<arr.length; i++){
+        if(arr[i]>largest){
+            secondLargest = largest;
+            largest = arr[i]
+        }else if(arr[i] !== largest &&  //if current number is not largest AND
+        (secondLargest===null || arr[i] > secondLargest)){ //we dont yet have secondLargest OR current is better than secondLargest
+            secondLargest = arr[i] //update secondLargest
+        }
+    }
+    return secondLargest
+    
+    
+}
+
+console.log(secondLargest([2,3,5,7,4,4,9,8]))
+console.log(secondLargest([5,5,5]))        
+console.log(secondLargest([5]))            
+console.log(secondLargest([-5, -3, -1]))   
+console.log(secondLargest([10, -3, -1]))   
+console.log(secondLargest([1, 2]))
+
+*/
